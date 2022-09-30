779a41fdba135ed9ff5533c3f6617c09a63ed308
Arif Ayarci <97611951+ayarcia1@users.noreply.github.com>
2022-09-29 22:26:30 -0400

    Added Java Swing

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,24 @@
+package src;
+import javax.swing.*;
+import java.awt.*;
 
+public class Main{
+    public static void main(String [] args){
+        new Main();
+    }
+
+    public Main(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button = new JButton("New Game");
+        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Minesweeper");
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
